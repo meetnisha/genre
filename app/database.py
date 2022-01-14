a97ea73bd961ac2dae8863fab1955fd8a5cc1667
Manisha Pradhan <manisha@usec.io>
2022-01-15 05:11:40 +0800

    chore: Data render complete

--- app/database.py
+++ app/database.py
@@ -8,6 +8,6 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-conn = engine.connect()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
+from pathlib import Path
 from datetime import datetime
 from . import models
 from fastapi import FastAPI, Request, Depends, BackgroundTasks, File, UploadFile
 from fastapi.templating import Jinja2Templates
-from .database import conn, engine
-from pydantic import BaseModel 
+from .database import SessionLocal, engine
+from pydantic import BaseModel
 from .models import Genre
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 import pandas as pd
 import io
-import datetime
+from starlette.responses import RedirectResponse
 app = FastAPI()
-from pathlib import Path
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -19,72 +20,85 @@ templates = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
 #templates = Jinja2Templates(directory="templates")
 
+
 class GenreRequest(BaseModel):
     symbol: str
 
 
 def get_db():
     try:
-        db = conn()
+        db = SessionLocal()
         yield db
     finally:
         db.close()
+
 
 """ @app.get("/")
 def index():
     return {"title": "Hello Coder"} """
 
-@app.get("/")
-def home(request: Request):
-    query = "SELECT * FROM genres"
-    cur = conn.cursor()
-    cur.execute(query)
-    genres = cur.fetchall()
 
-    """ genres = conn.query(Genre)    
-    genres = genres.all() """
-    
+@app.get("/")
+def home(request: Request, db: Session = Depends(get_db)):
+    genres = db.query(Genre)
+    genres = genres.all()
+    print(genres)
     return templates.TemplateResponse("home.html", {
-        "request": request, 
+        "request": request,
         "genres": genres
     })
 
 
 def classify_genres(df: object, current: object):
-    # assign data of lists.  
-    data = {'trackID': [20, 21, 19, 18], 'title': ['Tom', 'Joseph', 'Krish', 'John'],
-    'genre': ['classic', 'rock', 'sufi', 'romantic'], 'created': [current, current, current, current]}  
-    
-    # Create DataFrame  
-    df_cls = pd.DataFrame(data)  
+    # assign data of lists.
+    data = {'trackID': [22, 23, 14, 15], 'title': ['Tom1', 'Joseph2', 'Krish3', 'John4'],
+            'genre': ['classic', 'rock', 'sufi', 'romantic'], 'created': [current, current, current, current]}
+
+    # Create DataFrame
+    df_cls = pd.DataFrame(
+        data, columns=['trackID', 'title', 'genre', 'created'])
     return df_cls
 
+
 @app.post("/upload/")
-async def form_post(excel: UploadFile = File(...)):
+async def form_post(request: Request, excel: UploadFile = File(...), db: Session = Depends(get_db)
+                    ):
     """Uploads the file and processes it in Pandas"""
     contents = await excel.read()
-    test_data = io.BytesIO(contents)
+    test_data = io.BytesIO(contents)    
     df = pd.read_csv(test_data, sep=";")
-    current = datetime.datetime.now()
-    df_cls = classify_genres(df,current)
-    df_cls.to_sql("genres", con=conn, if_exists='replace')
+    current = datetime.now()
+    df_cls = classify_genres(df, current)
 
-    # genre = Genre()
-    # genre.id = "123"
-    # genre.genre = "Rap"
-    # db.add(genre)
-    # db.commit()        
+    # with SessionLocal.begin() as session:
+    #db.bulk_insert_mappings(Genre, df_cls)
+    objects = []
+    for idx, item in df_cls.iterrows():
+        one_object = Genre(
+            trackID=item[0], title=item[1], genre=item[2], created=item[3])
+        objects.append(one_object)
 
-    #background_tasks.add_task(classify_genres, df)
-    query = "SELECT * FROM genres WHERE created={} and ".format(current)
-    cur = conn.cursor()
-    cur.execute(query)
-    results = await cur.fetchall()
+    try:
+        db.bulk_save_objects(objects)
+        db.commit()
+    except:
+        return {
+            "code": "error",
+            "message": "Some or all trackIds have been classified already."
+        }
 
-    
-    #results = await conn.fetch_all(query=query)
+    """ # query from a class
+    statement = select(Genre).filter_by(created=current)
 
-    return  results
+    # list of first element of each row (i.e. Genre objects)
+    results = db.execute(statement).scalars().all()
+    message = {"code": "success",
+            "message": "genre was added to the database"}
+    print(message) """
+
+
+    return RedirectResponse(url="/", status_code=302)
+
 
 """ @app.post("/upload")
 async def find_genre(background_tasks: BackgroundTasks, csv_file: UploadFile = File(...), db: Session = Depends(get_db)):
